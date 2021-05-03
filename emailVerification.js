490f847f4bf9e25edd50ea62c0b4de656d77a3ca
aniketdevarkar <46354537+aniketdevarkar@users.noreply.github.com>
2021-05-03 14:57:16 +0530

    Update emailVerification.js

--- emailVerification.js
+++ emailVerification.js
@@ -1,36 +1,15 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const { google } = require("googleapis");
-const OAuth2 = google.auth.OAuth2;
-
-const oauth2Client = new OAuth2(
-  process.env.CLIENT_ID, // ClientID
-  process.env.CLIENT_SECRET, // Client Secret
-  "https://developers.google.com/oauthplayground" // Redirect URL
-);
-
-oauth2Client.setCredentials({
-  refresh_token: process.env.REFRESH_TOKEN,
-});
-const accessToken = oauth2Client.getAccessToken();
-
-const smtpTransport = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    type: "OAuth2",
-    user: "jigneshkapadia27@gmail.com",
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken: process.env.REFRESH_TOKEN,
-    accessToken: accessToken,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
-
 function emailVerification(token, email) {
+  let mailTransporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.user,
+      pass: process.env.pass,
+    },
+  });
+
   let mailOptions = {
     from: "jigneshkapadia27@gmail.com",
     to: `${email}`,
@@ -38,23 +17,43 @@ function emailVerification(token, email) {
     text: `click on the link to verify account
     https://amazon-clone-back.herokuapp.com/register/confirmation/${token}`,
   };
-  smtpTransport.sendMail(mailOptions, (error, response) => {
-    error ? console.log(error) : console.log(response);
-    smtpTransport.close();
+
+  mailTransporter.sendMail(mailDetails, function (err, data) {
+    if (err) {
+      console.log(err);
+      return err;
+    } else {
+      console.log("email sent");
+      return "1";
+    }
   });
 }
 
-async function resetPassword(token, email) {
+function resetPassword(token, email) {
+  let mailTransporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.user,
+      pass: process.env.pass,
+    },
+  });
+
   let mailOptions = {
     from: "jigneshkapadia27@gmail.com",
     to: `${email}`,
     subject: "Amazon Clone: Reset Password",
     text: `click on the link to reset password
-       https://amazon-clone-front.herokuapp.com/reset-password/${token}`,
+       https://amazonclone-aniket.netlify.app/reset-password/${token}`,
   };
-  smtpTransport.sendMail(mailOptions, (error, response) => {
-    error ? console.log(error) : console.log(response);
-    smtpTransport.close();
+
+  mailTransporter.sendMail(mailDetails, function (err, data) {
+    if (err) {
+      console.log(err);
+      return err;
+    } else {
+      console.log("email sent");
+      return "1";
+    }
   });
 }
 
